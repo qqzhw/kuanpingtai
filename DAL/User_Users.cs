57e5bc5f1dfaf3e27499c8abdbf9ffdab9d1256a
qqzhw <285665394@qq.com>
2017-05-22 23:20:39 +0800

    添加dal

--- DAL/User_Users.cs
+++ DAL/User_Users.cs
@@ -1,19 +1,31 @@
-﻿using System;
+﻿using DAL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using Model;
 
 namespace DAL
 {
     /// <summary>
     /// Business entity used to DAL User_Users
     /// </summary>
-    public class User_Users
+    public class User_Users : IUser_Users
     {
         //public DataTable GetAll()
         //{
         //    //TO DO....
         //}
+        public List<User_UsersInfo> GetAllUser(int pageIndex = 0, int pageSize = int.MaxValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User_UsersInfo GetUserInfoById(int userId)
+        {
+            throw new NotImplementedException();
+        }
     }
+
 }
