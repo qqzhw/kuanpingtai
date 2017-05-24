7fb3dc540dba370ffefaf287468fb9ed414bb755
qqzhw <285665394@qq.com>
2017-05-24 23:21:12 +0800

    加入实体

---
+++ Model/Shop_ShopsInfo.cs
@@ -0,0 +1,40 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    [Table("Shop")]
+    public class Shop_ShopsInfo
+    {
+        [Key]
+        public int ShopId { get; set; }
+        public string ShopName { get; set; }
+
+        public string ShopType { get; set; }
+       
+        public float Price { get; set; }
+
+        public float Commission { get; set; }
+        public string Img { get; set; }
+
+        public int State { get; set; }
+
+        public bool ShowOnHomePage { get; set; }
+
+         public int DisplayOrder { get; set; }
+
+        public int CreateUserId { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime LastDate { get; set; }
+
+        public string ComId { get; set; }
+
+        public int BmId { get; set; }       
+
+    }
+}
--- Model/User_UsersInfo.cs
+++ Model/User_UsersInfo.cs
@@ -12,6 +12,33 @@ namespace Model
     [Table("Users")]
     public class User_UsersInfo
     {
+        [Key]
         public int UserId { get; set; }
+        public int RoleID { get; set; }
+        public int DeptId { get; set; }
+        public string UserName { get; set; }
+        public string UserPwd { get; set; }
+        public string RealName { get; set; }
+        public string PhoneNo { get; set; }
+        public string Icon { get; set; }
+        public string ComId { get; set; }
+        public string Email { get; set; }
+        public string AliAccount { get; set; }
+        public decimal MyMoney { get; set; }
+        public int UserStatus { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastDate { get; set; }
+        public string LastIP { get; set; }
+        public string ComName { get; set; }
+        public string WorkArea { get; set; }
+        public string Modules { get; set; }
+
+        public int HandleNum { get; set; }
+
+        public int ClassId { get; set; }
+        public int UserType { get; set; }
+
+        public int BmId { get; set; }
+
     }
 }
